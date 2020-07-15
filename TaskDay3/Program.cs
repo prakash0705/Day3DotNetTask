4766f34d7205a00bbff3703015c97ede417fd258
Kumaran <Prakash.Kumaran@Chubb.com>
2020-07-15 18:09:59 +0530

    first commit

---
+++ TaskDay3/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+namespace TaskDay3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //To Get all User information
+            UserRepository repository = new UserRepository();
+            List<User> userlist = repository.Users();
+            Console.WriteLine("UserDetails\n");
+            Console.WriteLine("ID\t UserName\t UserEmail\t UserLocation\t UserAddress\t isActive");
+            foreach (User user in userlist)
+            { 
+                Console.WriteLine(user.Id + "\t" + user.Name + "\t" + user.Email + "\t" + user.Location + "\t" + user.Address + "\t\t" + user.IsActive);
+            }
+
+            //To find particular user based on id
+            Console.WriteLine("\nEnter the id to find information");
+            int id=int.Parse(Console.ReadLine());
+            User findUser = repository.GetUser(id);
+            
+            if(findUser!=null)
+            {
+                Console.WriteLine("ID\t UserName\t UserEmail\t UserLocation\t UserAddress\t isActive");
+                Console.WriteLine(findUser.Id + "\t" + findUser.Name + "\t" + findUser.Email + "\t" + findUser.Location + "\t" + findUser.Address + "\t" + findUser.IsActive);
+            }
+            else
+            {
+                Console.WriteLine("\nNo User found at specified id: " + id);
+            }
+            
+            
+            //To delete user based on id
+            Console.WriteLine("\n Enter the id to delte the user");
+            int deleteId = int.Parse(Console.ReadLine());
+            List<User> userList1 = repository.DeleteUser(deleteId);
+            Console.WriteLine("\nUser list information");
+            Console.WriteLine("ID\t UserName\t UserEmail\t UserLocation\t UserAddress\t isActive");
+            foreach (User user in userList1)
+            {
+
+                Console.WriteLine(user.Id + "\t" + user.Name + "\t" + user.Email + "\t" + user.Location + "\t" + user.Address + "\t" + user.IsActive);
+            }
+            
+            //Active users
+            Console.WriteLine("\nActive Users are:");
+            List<User> activeUserList = repository.ActiveUsers();
+            Console.WriteLine("ID\t UserName\t UserEmail\t UserLocation\t UserAddress\t isActive");
+            foreach (User user in activeUserList)
+            {
+                Console.WriteLine(user.Id + "\t" + user.Name + "\t" + user.Email + "\t" + user.Location + "\t" + user.Address + "\t" +user.IsActive);
+            }
+            
+            //Add user to list
+            Console.WriteLine("\nEnter the user Details");
+            Console.WriteLine("Enter the name");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Enter email");
+            string email = Console.ReadLine();
+            Console.WriteLine("Enter Location");
+            string location = Console.ReadLine();
+            Console.WriteLine("Enter Address");
+            string address = Console.ReadLine();
+            Console.WriteLine("Enter the User active status");
+            Console.WriteLine("0 - inactive \t 1 - active");
+            int activeStatus = int.Parse(Console.ReadLine());
+            repository.AddUser(new User(){
+
+                Id = repository.userId,
+                Name = userName,
+                Email = email,
+                Location = location,
+                Address = address,
+                IsActive = activeStatus
+            });
+            
+            //Display list after adding user
+            List<User> newList = repository.Users();
+            Console.WriteLine("ID\t UserName\t UserEmail\t UserLocation\t UserAddress\t isActive");
+            foreach (User user in newList)
+            {
+                Console.WriteLine(user.Id + "\t" + user.Name + "\t" + user.Email + "\t" + user.Location + "\t" + user.Address + "\t" + user.IsActive);
+            }
+        }
+    }
+}
---
+++ TaskDay3/UserRepository.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace TaskDay3
+{
+    public class UserRepository : User,IUserRepository
+    {
+        public int userId = 1;
+        private List<User> list = new List<User>();
+
+        public UserRepository()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                if(i%2!=0)
+                {
+                    list.Add(new User()
+                    {
+                        Id = userId,
+                        Name = "Prakash" + i,
+                        Email = "prakash" + i + "@gmail.com",
+                        Location = "location" + i,
+                        Address = "street" + i,
+                        IsActive = 1
+                    });
+                    userId++;
+                }
+                else
+                {
+                    list.Add(new User()
+                    {
+                        Id = userId,
+                        Name = "Prakash" + i,
+                        Email = "prakash" + i + "@gmail.com",
+                        Location = "location" + i,
+                        Address = "street" + i,
+                        IsActive = 0
+                    });
+                    userId++;
+                }
+            }
+        }
+
+
+
+       public List<User> Users()
+        {
+            return list;
+        }
+        public User GetUser(int id)
+        {
+            var user = list.Where(a => a.Id == id).FirstOrDefault();
+            return user;
+        }
+        public List<User> DeleteUser(int id)
+        {
+            var removeitem = list.Where(a => a.Id == id).FirstOrDefault();
+            if(removeitem==null)
+            {
+                Console.WriteLine("No User found at id: "+id);
+            }
+            else
+            {
+                list.Remove(removeitem);
+            }
+            return list;            
+        }
+        public List<User> ActiveUsers()
+        {
+            List<User> ActiveUserList = new List<User>();
+            foreach(var user in list)
+            {
+                if(user.IsActive==1)
+                {
+                    ActiveUserList.Add(user);
+                }
+            }
+            return ActiveUserList;
+        }
+        public List<User> AddUser(User user)
+        {
+            list.Add(user);
+            return list;
+        }
+    }
+}
